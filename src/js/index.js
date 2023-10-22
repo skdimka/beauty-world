870ea9c6581364a566145b2026c6fe688e27d5c6
Dmitry Skopintsev <skopintsevdima@gmail.com>
2023-10-22 18:21:41 +0600

    add carusel swiper and Webpack

--- src/js/index.js
+++ src/js/index.js
@@ -44,4 +44,24 @@ function handleFormSubmit(event) {
 
 
 
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
 
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
