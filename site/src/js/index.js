4c12d5975265ada07f2a79011f889db9eed0c740
Dmitry Skopintsev <skopintsevdima@gmail.com>
2023-11-02 01:38:35 +0700

    code refactoring

--- site/src/js/index.js
+++ site/src/js/index.js
@@ -1,85 +1,31 @@
+//у меня все таки почему то не работает импорт fancybox
+//index.js:1 Uncaught SyntaxError: Cannot use import statement outside a module (at index.js:1:1)
 
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
+// import Fancybox from "@fancyapps/ui";
+// import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
-
-
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
-document.getElementById("defaultOpen").click();
+// Fancybox.bind("[data-fancybox]", {});
+// Fancybox.bind('[data-fancybox="gallery"]', {});
 
 
+//здесь ошибка такая: main.min.js:14 Uncaught ReferenceError: Swiper is not defined
+    // at main.min.js:14:14
+    // at main.min.js:23:12
+
+//вроде самое простое, что можно сделать, но не работает(((
+
+// import Swiper from 'swiper';
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+// import 'swiper/css';
 
 
+// const swiper = new Swiper(".swiper", {
+//     slidesPerView: "4",
+//     direction: "horizontal",
+//     loop: true,
 
-//form
-
-const applicantForm = document.getElementById('form1')
-applicantForm.addEventListener('submit', handleFormSubmit)
-
-function handleFormSubmit(event) {
-    event.preventDefault()
-
-    const { name, phoneNumber } = event.target;
-
-    const User = {
-        name: name.value,
-        phoneNumber: phoneNumber.value
-    };
-
-    console.log(User);
-    document.getElementById("form1").reset();
-}
-
-const anchors = document.querySelectorAll(`a[href*= "#"]`)
-
-for (let anchor of anchors) {
-    anchor.addEventListener("click", function (event) {
-        event.preventDefault();
-        const blockID = anchor.getAttribute("href")
-        document.querySelector(" " + blockID).scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-        })
-    })
-}
-
-
-const appForm = document.getElementById('form2')
-appForm.addEventListener('submit', addFormSubmit)
-
-function addFormSubmit(event) {
-    event.preventDefault()
-    console.log('click')
-
-    const { name, phoneNumber, selectStaff, selectServices, data } = event.target;
-
-    const User = {
-        name: name.value,
-        phoneNumber: phoneNumber.value,
-        selectStaff: selectStaff.value,
-        selectServices: selectServices.value,
-        data: data.value,
-
-
-    };
-
-    console.log(User);
-    document.getElementById("form2").reset();
-}
+//     navigation: {
+//         nextEl: ".button-next",
+//         prevEl: ".button-prev",
+//     },
+// });
---
+++ site/src/js/modalForm.js
@@ -0,0 +1,22 @@
+const appForm = document.getElementById('form2')
+appForm.addEventListener('submit', addFormSubmit)
+
+function addFormSubmit(event) {
+    event.preventDefault()
+    console.log('click')
+
+    const { name, phoneNumber, selectStaff, selectServices, data } = event.target;
+
+    const User = {
+        name: name.value,
+        phoneNumber: phoneNumber.value,
+        selectStaff: selectStaff.value,
+        selectServices: selectServices.value,
+        data: data.value,
+
+
+    };
+
+    console.log(User);
+    document.getElementById("form2").reset();
+}
