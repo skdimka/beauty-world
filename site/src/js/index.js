f6673458e0bd2f120f8ecd431ed85b8bb2c67d7f
Dmitry Skopintsev <skopintsevdima@gmail.com>
2023-11-01 20:13:30 +0700

    reading data from a form

--- site/src/js/index.js
+++ site/src/js/index.js
@@ -24,6 +24,12 @@ function openCity(evt, cityName) {
 
 document.getElementById("defaultOpen").click();
 
+
+
+
+
+//form
+
 const applicantForm = document.getElementById('form1')
 applicantForm.addEventListener('submit', handleFormSubmit)
 
@@ -52,4 +58,28 @@ for (let anchor of anchors) {
             block: "start",
         })
     })
+}
+
+
+const appForm = document.getElementById('form2')
+appForm.addEventListener('submit', addFormSubmit)
+
+function addFormSubmit(event) {
+    event.preventDefault()
+    console.log('click')
+
+    const { name, phoneNumber, selectStaff, selectServices, data } = event.target;
+
+    const User = {
+        name: name.value,
+        phoneNumber: phoneNumber.value,
+        selectStaff: selectStaff.value,
+        selectServices: selectServices.value,
+        data: data.value,
+
+
+    };
+
+    console.log(User);
+    document.getElementById("form2").reset();
 }
